291bf606c4e3cd9555f0191da707c0895ead8bb8
jaabarri <jaabarri1112@gmail.com>
2024-02-15 17:45:31 -0800

    experiment 4

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,67 +1,52 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+'use strict';
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+var capture;
+var drawingColor;
+var capturePaused = false;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(640 * .5, 480*.5);
+  capture = createCapture(VIDEO, function() {
+    // Callback function to handle successful video initialization
+    capture.size(width, height);
+    capture.hide(); // Hide the video feed as we will draw it on the canvas
+  });
+  drawingColor = color(255, 0, 0); // Initial drawing color (red)
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  // Draw the video feed onto the canvas
+  //image(capture, 0, 0, width, height);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+
+  // Copy a region from the video feed with a random offset when paused
+  if (capturePaused) {
+    var x1 = floor(random(width));
+    var y1 = floor(random(height));
+
+    var x2 = x1 + floor(random(-50, 20));
+    var y2 = y1 + floor(random(-10, 10));
+
+    var w = 60;
+    var h = 25;
+
+    copy(capture, x1, y1, w, h, x2, y2, w, h);
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  if (keyCode == DELETE || keyCode == BACKSPACE) clear();
+}
+
 function mousePressed() {
-    // code to run when mouse is pressed
+  // Toggle camera pause on mouse click
+  capturePaused = !capturePaused;
+  if (capturePaused) {
+    capture.pause();
+    capture.hide();
+  } else {
+    capture.play();
+    capture.show();
+  }
 }
